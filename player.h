cb634293d8c625a9d27dee9f9f94334204daca05
Wenxuan Hu <w472h671@ku.edu>
2020-09-08 22:04:45 -0500

    update file

--- player.h
+++ player.h
@@ -10,21 +10,22 @@ class Player
 {
 public:
   Player(int ShipNums);
-
+  //constructor
   void setGuessPlace(string guess);
-
+  //set the user guess place
   string getGuessPlace() const;
-
+  //get the user guess place
   void shooting(bool hit, string guess);
-
+  //show the place that user shoot
   bool GettingShot(string guess);
-
+  //show the place that user getting shoot
   board* getBoard() const;
-
+  //show the player's board include guess place, hit place, have been hit place
   ~Player();
+  //destructor
 private:
   int numofships;
   Board* m_Board =nullptr;
-
+  string historyguess;
 };
 #endif
