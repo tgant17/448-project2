c919da404f28f035667318883f872671cfed7d97
Wenxuan Hu <w472h671@ku.edu>
2020-09-10 20:57:02 -0500

    finish the update board function

--- player.cpp
+++ player.cpp
@@ -21,14 +21,24 @@ string Player::getGuessPlace() const//return the place have been guess
   return historyguess;
 }
 
-void Player::shooting(/*string UserGuess, bool Hit*/)//show the user shooting place
+void Player::shooting(string UserGuess, bool Hit)//show the user shooting place in other player's board
 {
-  //not finished yet
-  //need throw runtime message if the guess not working
+    m_board->otherPlayerBoard(UserGuess, Hit);//update other player's board
 }
 
-bool Player::GettingShot(/*string UserGuess*/)//show the user getting shot place
+bool Player::GettingShot(string UserGuess)//show the user getting shot place in user board
 {
-  //not finished yet
+  if(m_board->isOffArray(UserGuess))
+  {
+    return (m_board->m_playerViewBoard(UserGuess));//update player's board
+  }
+  else
+  {
+    throw(runtime_error("Out of Boundary! Please try again.\n"))
+  }
   //need throw runtime message if the guess not working
+}
+Board* Player::getBoard() const
+{
+  return m_board;
 }
